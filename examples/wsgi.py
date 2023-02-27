812354caebb9bb3de73dd30174122289fb39c61b
Naibasak <deryswill@163.com>
2023-02-27 18:25:12 +0800

    demo

--- examples/wsgi.py
+++ examples/wsgi.py
@@ -1,16 +1,21 @@
 # coding=utf-8
 from binks.utils import logger
+from binks.UrlRoutingMiddleware import mw
+
+@mw.route('/hello')
+def hello(environ):
+    return 200, "hello world!"
 
 def app(environ, start_response):
     """Simplest possible application object"""
-    data = b'Hello,World Binks\n'
-    status = '200 OK'
+    # logger.debug('environ:%s', environ)
+
+    path = environ['PATH_INFO']
+    status, data = mw.callRoute(path, environ)
     response_headers = [
         ('Content-type','application/json'),
         ('Content-Length', len(data))
     ]
-
-    logger.debug('environ:%s', environ)
 
     start_response(status, response_headers)
     return [data]
